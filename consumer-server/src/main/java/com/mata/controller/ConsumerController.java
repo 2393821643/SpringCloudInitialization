a0e8520cef6b0405197cab3edcebf5f12d1c387a
mataHape <2393821643@qq.com>
2024-02-22 15:56:43 +0800

    自建SpringCloud底座

--- consumer-server/src/main/java/com/mata/controller/ConsumerController.java
+++ consumer-server/src/main/java/com/mata/controller/ConsumerController.java
@@ -5,9 +5,12 @@ import com.mata.entity.Consumer;
 import com.mata.entity.Producer;
 import com.mata.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/consumer")
@@ -15,8 +18,12 @@ public class ConsumerController {
     @Autowired
     private ProducerClient producerClient;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("/getOneConsumer")
     private Result<Consumer> getOneConsumer(){
+        stringRedisTemplate.opsForValue().set("testKey","it is redis",2, TimeUnit.MINUTES);
         Producer producer = producerClient.getProducer();
         Consumer consumer = Consumer.builder()
                 .consumerId(5L)
